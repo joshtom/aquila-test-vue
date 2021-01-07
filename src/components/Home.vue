4f4fdeccfcfbfcfa016244f8bbb54c7f87b0e1a4
joshtom <olajidejoshua4real@gmail.com>
2021-01-07 16:16:51 +0100

    Bootstrap new vue project with vue cli

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Working {{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    // eslint-disable-next-line prettier/prettier
+    msg: String,
+    // eslint-disable-next-line prettier/prettier
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
